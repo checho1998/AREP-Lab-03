b39e161a946b0bdb2f692c4f20a3b89892b4dc25
Checho1998 <c.hecho.28@hotmail.com>
2020-01-31 12:44:28 -0500

    Entrega

--- src/main/java/edu/escuelaing/arep/AplicacionApp.java
+++ src/main/java/edu/escuelaing/arep/AplicacionApp.java
@@ -5,10 +5,38 @@
  */
 package edu.escuelaing.arep;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URL;
+
 /**
  *
  * @author sergio.nunez
  */
 public class AplicacionApp {
     
+    public static void main(String[] args) throws Exception {
+        Socket miSoket = null;
+
+        PrintWriter out = null;
+        
+
+        BufferedReader in = null;
+
+        try  {
+            
+            miSoket =  new  Socket("127.0.0.1", 35000);
+           
+            out =  new  PrintWriter(miSoket.getOutputStream(),  true); in =  new  BufferedReader(new  InputStreamReader(echoSocket.getInputStream()));
+           
+            }  catch  (Exception e)  {
+           
+            System.err.println("No sabe el puerto o el host");
+
+            }
+        
+    }
 }
